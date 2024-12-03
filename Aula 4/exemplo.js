420cda08a264d03f982e1847e1c7644f559aa1ef
Luana Maria <luanamariadaluzbarbosa@gmail.com>
2024-12-03 10:36:15 -0300

    aula 4 - if, else e for

---
+++ Aula 4/exemplo.js
@@ -0,0 +1,15 @@
+/* const anaVaiNa25DeMarco = true; // A Ana vai na 25 de Março 
+const temOtimasPromocoes = true; //Exitem ótimas promoções
+
+if (anaVaiNa25DeMarco && temOtimasPromocoes){
+    console.log("Ana fica radiante!");
+} */
+
+const anaVaiNa25DeMarco = true;
+const temOtimasPromocoes2 = false;
+
+if (anaVaiNa25DeMarco && temOtimasPromocoes2){
+    console.log("Ana fica radiante")
+} else {
+    console.log("Ana fica bem triste")
+}
